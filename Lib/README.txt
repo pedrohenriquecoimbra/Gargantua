83c04c33e2b2d3377e8780bbcab20d5815c5896c
Pedro Henrique H. Coimbra <49572701+pedrohenriquecoimbra@users.noreply.github.com>
2023-08-08 15:37:06 +0200

    push July
    unsupervised push

---
+++ Lib/README.txt
@@ -0,0 +1,1 @@
+Put in here any Gargantua-friendly libraries.
---
+++ __scripts__/versions.py
@@ -0,0 +1,4 @@
+version_name = "OpenFLUX"
+version_number = "V0.1.1"
+# V0.1.1 - cumulative changes up to 28/07/2023
+# V0.1.0 - put together corrections and wavelet flux processing
